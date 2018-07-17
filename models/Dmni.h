24db4e9cb15ce406df264ab48114081323285999
Anderson Domingues <ti.andersondomingues@gmail.com>
2018-07-16 22:36:29 -0300

    added some keep.me files and stub files for the dmni module.

---
+++ models/Dmni.h
@@ -0,0 +1,17 @@
+#ifndef __DMNI_H
+#define __DMNI_H
+
+
+class Dmni : Process {
+
+	private:
+	
+	
+	public:
+	  
+
+
+};
+
+
+#endif /* DMNI */
---
+++ models/src/Dmni.cpp
@@ -0,0 +1,12 @@
+#include <Dmni.h>
+#include <cstdlib>
+
+void Dmni::cycle(risc_v_state *s){
+
+
+}
+
+
+Dmni::~Dmni(){
+	int x = 1 + 1;
+}
