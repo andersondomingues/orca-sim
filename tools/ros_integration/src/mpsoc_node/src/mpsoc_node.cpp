12706c4176f720006e5521c411b64d5a015b9616
Anderson Domingues <ti.andersondomingues@gmail.com>
2018-12-04 15:59:41 -0200

    transition commit

--- tools/ros_integration/src/mpsoc_node/src/mpsoc_node.cpp
+++ tools/ros_integration/src/mpsoc_node/src/mpsoc_node.cpp
@@ -1,5 +1,5 @@
 #include "ros/ros.h"
-#include "geometry_msgs/Vector3.h"   // Motion
+#include "geometry_msgs/Point.h"   // Motion
 
 #include "../include/udp_client_server.h"
 
@@ -23,16 +23,90 @@
 ros::Publisher  pub_mpsoc_out;
 ros::Subscriber pub_mpsoc_in;
 
+//definition of topic type; Include directive
+//must be changed as well
+typedef geometry_msgs::Point topic_t ;
+
 //conection to udp network
 udp_client* uclient;
 udp_server* userver;
 
-//function that add noc headers to the buffer and 
-//return the initial address of the payload
-char* add_noc_headers(char* buffer){
+//shorthand print
+void dump(char* _mem, uint32_t base, uint32_t length){
+	uint32_t k, l;
 	
-	/* BEWARE THE ENDIANESSSSSS */
+	//mask is necessary to correct a bug(?) when printing
+	//negative hexas.
+	uint32_t mask = 0x000000FF; 
+	int8_t ch;
 	
+	//uint32_t* memptr = (uint32_t*)_mem;
+	//uint32_t  len = _length / 4;
+	for(k = 0; k < length; k += 16){
+		printf("\n%08x ", base + k);
+		for(l = 0; l < 16; l++){
+			printf("%02x ", _mem[k + l] & mask );
+			if (l == 7) putchar(' ');
+		}
+		printf(" |");
+		for(l = 0; l < 16; l++){
+			ch = _mem[k + l];
+			if ((ch >= 32) && (ch <= 126))
+				putchar(ch);
+			else
+				putchar('.');
+		}
+		putchar('|');
+	}
+}
+
+//serialize
+void* pack_data(char* addr, const topic_t::ConstPtr& msg){
+
+	char* ptr = addr;
+	
+	//add x val
+	uint32_t x_val = msg->x * 100000;
+	*(uint32_t*)ptr = x_val;
+	
+	//inc 4 bytes
+	ptr += 4;
+	
+	//add x val
+	uint32_t y_val = msg->y * 100000;
+	*(uint32_t*)ptr = y_val;
+	
+	ptr += 4;
+	
+	//add x val
+	uint32_t z_val = msg->z * 100000;
+	*(uint32_t*)ptr = z_val;
+
+}
+
+//unserialize
+topic_t unpack_data(char* addr){
+	
+	topic_t t;
+	
+	
+	ROS_INFO("unknown type, cannot unpack data");
+	
+}
+
+//Callback that consumes message on the input topic and process
+//information. Message is published back into output topic at
+//the end of processing.
+void mpsoc_in_callback(const topic_t::ConstPtr& msg){
+
+	//buffes must be in the callback, otherwise
+	//data will be overwritter by next iterations
+	char buffer[UDP_BUFFER_LEN];
+
+	for(int i = 0; i < UDP_BUFFER_LEN; i++)
+		buffer[i] = 0x00;
+
+	//add noc headers 
 	buffer[0] = 0x11;  //(1,1) is core #5
 	buffer[1] = 0x00; 
 
@@ -54,51 +128,52 @@ char* add_noc_headers(char* buffer){
 	buffer[12] = 0x01;
 	buffer[16] = 0x20;
 	
-	//TODO: double check headers 
-	return &buffer[17];
-}
-
-char* remove_noc_headers(char* buffer){
-	return &buffer[17];
-}
-
-//Callback that consumes message on the input topic and process
-//information. Message is published back into output topic at
-//the end of processing.
-void mpsoc_in_callback(const geometry_msgs::Vector3::ConstPtr& msg){
-
-	//buffes must be in the callback, otherwise
-	//data will be overwritter by next iterations
-	char buffer[UDP_BUFFER_LEN];
-
-	//add noc headers 
-	char* start_addr = add_noc_headers(buffer);
-	
-	//add payload (TODO: serialization)
 	for(int i = 0; i < 20; i++)
-		buffer[17 + i] = 0x64;
+		buffer[17 + i] = 0x63;
 	
-	buffer[40] = 0x0a;
+	buffer[40] = 0x0a; // \n
 	
-	//*(float*)start_addr = msg.getX();
-	//start_addr += 8;
-	
-	//*(float*)start_addr = msg.py;
-	//start_addr += sizeof(msg.py);
-	
-	//*(float*)start_addr = msg.pz;
-	//start_addr += sizeof(msg.pz);
+	//pack data
+	pack_data(&buffer[20], msg);
 	
 	//send via udp
 	uclient->send((const char*)buffer, UDP_BUFFER_LEN);
 	
-	//wait for response 
-	userver->recv(buffer, UDP_BUFFER_LEN);
+	dump(buffer, 0, UDP_BUFFER_LEN);
+	printf("\n");
 	
 	//do whatever has to be done with the data
 	//start_addr = remove_noc_headers(buffer);
-	ROS_INFO("lalala finished");		
+	ROS_INFO("data sent");
 }
+
+//Non-blocking function for receiveing packets from 
+//the network
+void* mpsoc_out(void* v){
+	
+	//buffes must be in the callback, otherwise
+	//data will be overwritter by next iterations
+	char buffer[UDP_BUFFER_LEN];
+	
+	while(1){
+	
+		//recv packet from the network
+		userver->recv(buffer, UDP_BUFFER_LEN);
+		
+		char* start_addr = &buffer[20];
+		
+		//unpack data
+		topic_t data;
+		data = unpack_data(start_addr);
+		
+		//publish through ros 
+		pub_mpsoc_out.publish(data);
+		
+		ROS_INFO("data recv'd");
+	
+	}
+}
+
 
 //Main routine: setup publishers and subscribers.
 int main(int argc,char **argv)
@@ -110,7 +185,7 @@ int main(int argc,char **argv)
 	//ROS_INFO();
 	//subscribe and advertise to input and output topics. 
 	pub_mpsoc_in  = n.subscribe("/mpsoc_in", 1, mpsoc_in_callback); 
-	pub_mpsoc_out = n.advertise<geometry_msgs::Vector3>("/mpsoc_out", 1);
+	pub_mpsoc_out = n.advertise<topic_t>("/mpsoc_out", 1);
 
 	//initialize udp bridge
 	const std::string& client_addr = MPSOC_ADDR;
@@ -118,6 +193,10 @@ int main(int argc,char **argv)
 
 	const std::string& server_addr = ROSNODE_ADDR;
 	userver = new udp_server(server_addr, ROSNODE_PORT);
+
+	//start receiving thread
+	pthread_t thread;
+	pthread_create(&thread, NULL, mpsoc_out, NULL);
 
 	ros::spin(); 
 
