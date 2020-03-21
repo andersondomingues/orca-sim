e0ba478efb8c328f629819813e21095598129574
Anderson Domingues <ti.andersondomingues@gmail.com>
2020-03-21 18:18:57 -0400

    added tutorial files for tutorial 1

---
+++ models/include/UMultiplier.h
@@ -0,0 +1,58 @@
+/** 
+ * This file is part of project URSA. More information on the project
+ * can be found at 
+ *
+ * URSA's repository at GitHub: http://https://github.com/andersondomingues/ursa
+ *
+ * Copyright (C) 2018 Anderson Domingues, <ti.andersondomingues@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. **/
+#ifndef __UMultiplier_H
+#define __UMultiplier_H
+
+//lib dependent includes
+#include <iostream>
+#include <queue>
+#include <stdint.h>
+
+//api includes
+#include <UntimedModel.h>
+
+/**
+ * @class Untimed Multiplier
+ * @author Anderson Domingues
+ * @date 03/21/30
+ * @file UMultiplier.h
+ * @brief Implements an untimed multiplier
+ * */
+class UMultiplier : public UntimedModel{
+
+private:
+    std::string _name;
+    uint32_t _opa;
+	uint32_t _opb;
+
+public:
+
+	UMultiplier(std::string name);
+	~UMultiplier();
+
+	void SetOpA(uint32_t opa);
+	void SetOpB(uint32_t opb);
+
+	uint32_t GetResult();
+};
+
+#endif /* UMultiplier_H */
