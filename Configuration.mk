348ad8b1fd096e598edf162587dbaca708ad6305
Anderson Domingues <ti.andersondomingues@gmail.com>
2020-04-16 03:22:17 -0300

    reworked Configuration.mk file, added friendly names to parameters, removed unused parameters, syntax cleanup, more comments. Changed to Makefile to reflect these changes

--- Configuration.mk
+++ Configuration.mk
@@ -1,13 +1,16 @@
-# =======================================================[ GLOBAL SETTINGS ]
-# Flags we set here will be propagated to all hardware modules (not sw)
-# Currently adopted flags:
-# -Wall, -Wextra, Werror: activate a lot of warning at compile time
-# -g: attach debug information to the generated executable
-# -std: required by old GCC to set c++17 as default the c++ 
-# -march, -mtune: optimize code for current machine architecture
-# -lasan, -fsanitize: add memory sanitizer to code
-GLOBAL_SETTINGS := -Wall -Wextra -Werror -g -std=c++17 -O3 -march=native -mtune=native 
-#-lasan -fsanitize=address
+#================================================================#
+# COMPILING OPTIONS                                              #
+#================================================================#
+
+# Below options will be set for all compilation within the
+# project. 
+# -Wall, -Wextra, -Werror: activates extra reporting 
+# -g: adds debugging information
+# -std=c++17: set C++ standard (required for old compilers)
+# -O3, -march=native, -mtune=native: optmizations
+# -lasan, -fsanitize=address: additional debugging support
+ORCA_GLOBAL_FLAGS := -Wall -Wextra -Werror -g -std=c++17 \
+	-O3 -march=native -mtune=native
 
 # Select the target platform. Different hardware architectures
 # are deployed to /platforms folder. We currently provide two
@@ -15,152 +18,157 @@ GLOBAL_SETTINGS := -Wall -Wextra -Werror -g -std=c++17 -O3 -march=native -mtune=
 # based on the HF-RiscV processor and (ii) a NoC-based 
 # mesh-topologic manycore that uses the same processor core. 
 # PLATFORM := (orca-dma | single-core)
-PLATFORM      := orca-dma
+ORCA_PLATFORM := orca-dma
 
-# Apps to be compiled within kernel image. For multiple applications, 
-# use spacebar to separate names. Applications defined here will not 
-# be included in compilation unless you edit the file 
-#          extensions/orca-core/src/orca-core.cpp,
-# where you should set the spawn of tasks in each of the cores. 
-ORCA_APPLICATIONS := producer-consumer-pubsub producer-consumer app-spawner app-bloater deadline-monitor
+#================================================================#
+# SIMULATION OPTIONS                                             #
+#================================================================#
 
-# Software extensions (experimental)
-ORCA_EXTENSIONS := orca-core orca-pubsub orca-monitoring
+# Number of cycles to be simulated before URSA's engine flush 
+# the simulation queue. Larger number may present more accurate
+# speed and a more accurate speed reporting, although consuming 
+# more memory. Must be between (TILES * 4) and MAX_INT.
+ORCA_EPOCH_LENGTH := 10000000
 
-# ============================================================[ HELLFIREOS ]
-# Set level of logging for the HellfireOS kernel. 
-# 0 => disabled 
-# 1 => interruption and dispatch information (default)
-# 2 => same as level one plus calls to kernel functions
-KERNEL_LOG_LEVEL := 1
+# Number of epochs to simulate. If set INF, simulation will run 
+# until it receives a CTRL+C (sigint) interruption. Please note
+# that one interruption is enough to stop the simulation, which 
+# may last until the end of the current epoch. Another interrupt-
+# ion will kill the application, skipping the reporting step
+# (sigkill). When set to a positive value, the simulation engine
+# execute for ORCA_EPOCH_LENGTH times ORCA_EPOCHS_TO_SIM cycles.
+ORCA_EPOCHS_TO_SIM := INF
 
-# ==================================================================[ ORCA ]
-# Width (x-axis coordinate) of the network-on-chip. Cannot be zero,
-# otherwise simulation won't compile.
+# Set this option to YES to check event queue for invalid events.
+# If not debugging scheduling, set this option to NO, as it deple-
+# tes simulation performance.
+ORCA_ZERO_TIME_CHECKING := NO
+
+# Set this option to YES to check whether the queue is empty 
+# during the simulation. If not debuggin scheduling, set this 
+# option to NO, as it depletes simulation time. 
+ORCA_QUEUE_SIZE_CHECKING := NO
+
+#================================================================#
+# NETWORK ON-CHIP OPTIONS                                        #
+#================================================================#
+
+# Height (maximum x-axis coordinate -1) of the network-on-chip.
+# Cannot be zero. Please note that the total number of tiles 
+# (ORCA_NOC_HEIGHT * ORCA_NOC_WIDTH) cannot exceed MAX_UINT16.
 ORCA_NOC_HEIGHT := 3
-# Width (y-axis coordinate) of the network-on-chip. Cannot be zero,
-# otherwise simulation won't compile.
+
+# Height (maximum y-axis coordinate -1) of the network-on-chip.
+# Cannot be zero. Please note that the total number of tiles 
+# (ORCA_NOC_HEIGHT * ORCA_NOC_WIDTH) cannot exceed MAX_UINT16.
 ORCA_NOC_WIDTH  := 2
 
-# Number of cycles before calling the frequency analisys tool. Shorter
-# values may compromise the performance of the simulation, while higher
-# values may provide inaccurate measurements of the achieved frequency.
-ORCA_EPOCH_LENGTH  := 10000000
-#ORCA_EPOCH_LENGTH  := 50000000
+# Configures the quantity of flits that buffers can store before
+# reaching overflow. Memory usage increases for large buffer, 
+# although not impacting in simulation performance.
+ORCA_BUFFER_CAPACITY := 8
 
-# Number of pulses to simulate. Set to INF to simulate indefinitely.
-ORCA_EPOCHS_TO_SIM := INF
-#ORCA_EPOCHS_TO_SIM := 3
+# Set it to YES to enforce the simulation to check for buffers'
+# sizes before pushing data (depletes simulation performance).
+ORCA_CHECK_BUFFER_OVERFLOW := NO
 
-# Enable multithread simulation (experimental)
-ORCA_ENABLE_MULTITHREADING := NO
+# Set it to YES to enforce the simulation to check for buffers'
+# sizes when pulling data. (depletes simulation performance). 
+ORCA_CHECK_BUFFER_UNDERFLOW := NO
 
-# ===========================================================[ URSA ENGINE ]
-# Check whether some event has been schedule to run in some point of 
-# time prior to the beggining. First event must always be schedule to 
-# time=1, otherwise the simulation will crash (depletes performance).
-URSA_ZERO_TIME_CHECKING := NO
+#================================================================#
+# OFF-CHIP COMMUNICATION OPTIONS                                 #
+#================================================================#
 
-# Check whether the queue has some event before simulating the next event.
-# When simulating hardware, the number of events in the queue is constant,
-# so checking the size of the queue is unecessary. Set this option to 
-# YES to force checking (depletes performance).
-URSA_QUEUE_SIZE_CHECKING := NO
+# Check whether destination port is connected when tranfering
+# flits. Transfering flit to routers not mapped into the topology
+# results will crash the simulator if this option if set to NO. 
+# Setting it YES depletes performance. 
+ORCA_CHECK_ROUTER_PORTS := NO
 
-# =============================================================[ NETSOCKET ]
-# Outputs log for outgoing packets (slightly depletes performance).
-NETSOCKET_LOG_OUTGOING_PACKETS := NO
+# These options control the log for packets passing through the 
+# off-chip communication interface. Set this option to NO to
+# disable logging, MINIMAL to print short messages, or FULL to 
+# display the content of every packet. Please note that this may
+# slow down the simulation.
+ORCA_OFFCHIP_LOG_IN  := NO
+ORCA_OFFCHIP_LOG_OUT := NO
 
-# Outputs log for incoming packets (slightly depletes performance).
-NETSOCKET_LOG_INCOMING_PACKETS := NO
+# Sets the IP address for the server that handles packets coming
+# from outside the chip. All tiles handling packets will share 
+# the same server address. Setting this to \"127.0.0.1\" must 
+# work for most systems. 
+ORCA_OFFCHIP_SERVER_IP := \"127.0.0.1\"
 
-NETBRIDGE_ENABLE_LOG_INPUT  := YES
-NETBRIDGE_ENABLE_LOG_OUTPUT := YES
+# Sets the base port number for servers handling packets from
+# outside the chip. The port to be used for each server is an
+# increment on the base port. For example, core #2 will serve
+# port 5002 if base port is set to 5000.
+ORCA_OFFCHIP_SERVER_PORT  := 8888
 
-# Sets client UDP/IP address and port for the netsocket. This is the address
-# that the netsocket will connect when sending packets to outside the noc.
-NETSOCKET_CLIENT_ADDRESS := \"127.0.0.1\"
-NETSOCKET_CLIENT_PORT := 8888
+# Sets the IP address to where the packets going to outside the
+# chip must be delivered. Set it to \"127.0.0.1\" if running
+# the client application in the same host as the simulation
+ORCA_OFFCHIP_CLIENT_IP := \"127.0.0.1\"
+ORCA_OFFCHIP_CLIENT_PORT := 9999
 
-# Sets server IP address and port for the netsocket. This is the address
-# that application would use to send packets to the internal network.
-NETSOCKET_SERVER_ADDRESS := \"127.0.0.1\"
-NETSOCKET_SERVER_PORT := 9999
+#================================================================#
+# MEMORY CORE OPTIONS                                            #
+#================================================================#
 
-# ===============================================================[ BUFFER ]
-# Check whether the buffer is full before pushing data (depletes performance).
-BUFFER_OVERFLOW_CHECKING := NO
+# Lowest readable/writable memory address. This option applies to
+# all memory cores in the platform.
+ORCA_MEMORY_BASE := 0x40000000
 
-# Check whether the buffer is empty before popping data (depletes performance).
-BUFFER_UNDERFLOW_CHECKING := NO
+# Memory size (in bytes). This option applies to all memory cores
+# in the platform. Syntax must be one of the following:
+# 1000000     -- nearly 1MB sized 
+# 0xFFFFF     -- exactly 1MB sized
+# pow(10,6)   -- same as 1000000 (~1MB)
+ORCA_MEMORY_SIZE := 1000000
 
-# Configure the capacity of the buffers used within the system. To disable 
-# network congestion, set this to a higher value. Please note that increasing t
-# the capacity of buffers also increases the memory usage of the simulator. (
-# The default for this option is 8 flits.
-BUFFER_CAPACITY := 8
+# Set this option to YES to force the simulator to check the li-
+# mits of memory space when reading or writing. This option is 
+# useful for debugging memory access, but dramatically depletes
+# simulation performance. 
+ORCA_CHECK_MEMORY_SPACE := NO
 
-# ===============================================================[ MEMORY ]
-# Check whether address are mapped to some memory range before writing
-# to memory. Set to YES to force checking (depletes performance).
-MEMORY_WRITE_ADDRESS_CHECKING := NO
+#================================================================#
+# HF-RISCV CORE OPTIONS                                          #
+#================================================================#
 
-# Check whether address are mapped to some memory range before reading from
-# memory. Set to YES to force checking (depletes performance).
-MEMORY_READ_ADDRESS_CHECKING := NO
+# Set the operation mode for the HFRisc-V core. Two modes are 
+# supported (INSTRUCTION is faster):
+# - CYCLE: mimic pipeline by adding delay to instructions 
+# - INSTRUCTION: all instruction take exacly one cycle to finish 
+ORCA_HFRISCV_MODE := CYCLE
 
-# Check whether address are mapped to some memory range before wipeing
-# memory ranges. Set to YES to force checking (depletes performance).
-MEMORY_WIPE_ADDRESS_CHECKING := NO
+#================================================================#
+# HARDWARE COUNTERS                                              #
+#================================================================#
 
-# Enable counter for read and write operations (depletes performance).
-MEMORY_ENABLE_COUNTERS := NO
+# Enable hardware counter for several hardware components. These 
+# counter can be used to estimate the energy consumption of the 
+# platform. Enabling counters depletes performance.
 
-# ==============================================================[ HFRISCV ]
-# Check whether address are mapped to some memory range before writing
-# to memory. Set to YES to force checking (depletes performance). This
-# option does not override the one in memory module.
-# TODO: check whether this options works
-HFRISCV_WRITE_ADDRESS_CHECKING := NO
+# Counts the number cycle in which the router tranfers flits.
+ORCA_HWCOUNTERS_ROUTERS := NO
 
-# Check whether address are mapped to some memory range before reading
-# from memory. Set to YES to force checking (depletes performance). This
-# option does not override the one in memory module.
-# TODO: check whether this options works
-HFRISCV_READ_ADDRESS_CHECKING := NO
+# Counts the number of readings and writings.
+ORCA_HWCOUNTERS_MEMORY := NO
 
-# Enable counter for instructions' classes (depletes performance).
-HFRISCV_ENABLE_COUNTERS := YES
+# Counts the number of instructions per instruction class.
+ORCA_HWCOUNTERS_HFRISCV := NO
 
-# Set the operation mode for the core, can be 
-# either CYCLE or INSTRUCTION (default).
-# - CYCLE: enable cycle counting per instruction and branch prediction
-# - INSTRUCTION: all instruction take one cycle to exit the pipeline
-#HFRISCV_MODE := CYCLE
-HFRISCV_MODE := INSTRUCTION
+#================================================================#
+# GENERATION OF COMPILATION PARAMETERS STARTS HERE.              #
+# DO NOT MODIFY BELOW THIS LINE!                                 # 
+#================================================================#
 
-# ==============================================================[ NETIF ]
-# Check whether netif is writing to unmapped memory space
-NETIF_WRITE_ADDRESS_CHECKING := NO
+#ORCA_GLOBAL_FLAGS
+COMPLINE := $(ORCA_GLOBAL_FLAGS)
 
-# check whether netif is reading from unmapped memory space
-NETIF_READ_ADDRESS_CHECKING := NO
-
-# ==============================================================[ ROUTER ]
-# Enable counters for number of active cycles
-ROUTER_ENABLE_COUNTERS := NO
-
-# Check whether destination port is connected when tranfering flits.
-# Transfering flit to routers not mapped into the topology results in
-# crash. Set to YES to force checking (depletes performance).
-ROUTER_PORT_CONNECTED_CHECKING := NO
-
-# ========================================================================
-# GENERATION OF COMPILATION PARAMETERS STARTS HERE.
-# DO NOT MODIFY BELOW THIS LINE!
-# ========================================================================
-
-#ORCA parameters
+#simulation flags
 ifneq ($(ORCA_EPOCHS_TO_SIM), INF)
 	COMPLINE := $(COMPLINE) -DORCA_EPOCHS_TO_SIM=$(ORCA_EPOCHS_TO_SIM)
 endif
@@ -170,100 +178,73 @@ COMPLINE := $(COMPLINE) \
 	-DORCA_NOC_WIDTH=$(ORCA_NOC_WIDTH) \
 	-DORCA_EPOCH_LENGTH=$(ORCA_EPOCH_LENGTH)
 
-ifeq ($(ORCA_ENABLE_MULTITHREADING), YES)
-	COMPLINE := $(COMPLINE) -DORCA_ENABLE_MULTITHREADING
-endif 
-
-#URSA parameters
-ifeq ($(URSA_ZERO_TIME_CHECKING), YES)
+#URSA parameters (code uses URSA instead of ORCA)
+ifeq ($(ORCA_ZERO_TIME_CHECKING), YES)
 	COMPLINE := $(COMPLINE) -DURSA_ZERO_TIME_CHECKING
 endif
-ifeq ($(URSA_QUEUE_SIZE_CHECKING), YES)
+ifeq ($(ORCA_QUEUE_SIZE_CHECKING), YES)
 	COMPLINE := $(COMPLINE) -DURSA_QUEUE_SIZE_CHECKING
 endif
 
-#netbridge parameters
-ifeq ($(NETBRIDGE_ENABLE_LOG_INPUT), YES)
-	COMPLINE := $(COMPLINE) -DNETBRIDGE_ENABLE_LOG_INPUT
-endif
-ifeq ($(NETBRIDGE_ENABLE_LOG_OUTPUT), YES)
-	COMPLINE := $(COMPLINE) -DNETBRIDGE_ENABLE_LOG_OUTPUT
-endif
-
-#netsocket parameters
-ifeq ($(NETSOCKET_LOG_OUTGOING_PACKETS), YES)
-	COMPLINE := $(COMPLINE) -DNETSOCKET_LOG_OUTGOING_PACKETS
-endif
-ifeq ($(NETSOCKET_LOG_INCOMING_PACKETS), YES)
-	COMPLINE := $(COMPLINE) -DNETSOCKET_LOG_INCOMING_PACKETS
-endif
-
-COMPLINE := $(COMPLINE) \
-	-DNETSOCKET_CLIENT_ADDRESS=$(NETSOCKET_CLIENT_ADDRESS) \
-	-DNETSOCKET_CLIENT_PORT=$(NETSOCKET_CLIENT_PORT) \
-	-DNETSOCKET_SERVER_ADDRESS=$(NETSOCKET_SERVER_ADDRESS) \
-	-DNETSOCKET_SERVER_PORT=$(NETSOCKET_SERVER_PORT)
-
-#netif parameters
-ifeq ($(NETIF_WRITE_ADDRESS_CHECKING), YES)
-	COMPLINE := $(COMPLINE) -DNETIF_WRITE_ADDRESS_CHECKING
-endif
-ifeq ($(NETIF_READ_ADDRESS_CHECKING), YES)
-	COMPLINE := $(COMPLINE) -DNETIF_READ_ADDRESS_CHECKING
-endif
-
-
 #buffer parameters
-ifeq ($(BUFFER_OVERFLOW_CHECKING), YES)
+ifeq ($(ORCA_BUFFER_OVERFLOW_CHECKING), YES)
 	COMPLINE := $(COMPLINE) -DBUFFER_OVERFLOW_CHECKING
 endif
-ifeq ($(BUFFER_UNDERFLOW_CHECKING), YES)
+ifeq ($(ORCA_BUFFER_UNDERFLOW_CHECKING), YES)
 	COMPLINE := $(COMPLINE) -DBUFFER_UNDERFLOW_CHECKING
 endif
 
-COMPLINE := $(COMPLINE) \
-	-DBUFFER_CAPACITY=$(BUFFER_CAPACITY)
+COMPLINE := $(COMPLINE) -DBUFFER_CAPACITY=$(ORCA_BUFFER_CAPACITY)
 
-#memory parameters
-ifeq ($(MEMORY_WRITE_ADDRESS_CHECKING), YES)
-	COMPLINE := $(COMPLINE) -DMEMORY_WRITE_ADDRESS_CHECKING
-endif
-ifeq ($(MEMORY_READ_ADDRESS_CHECKING), YES)
-	COMPLINE := $(COMPLINE) -DMEMORY_READ_ADDRESS_CHECKING
-endif
-ifeq ($(MEMORY_WIPE_ADDRESS_CHECKING), YES)
-	COMPLINE := $(COMPLINE) -DMEMORY_WIPE_ADDRESS_CHECKING
-endif
-ifeq ($(MEMORY_ENABLE_COUNTERS), YES)
-	COMPLINE := $(COMPLINE) -DMEMORY_ENABLE_COUNTERS
-endif
-
-#hfriscv parameters
-ifeq ($(HFRISCV_WRITE_ADDRESS_CHECKING), YES)
-	COMPLINE := $(COMPLINE) -DHFRISCV_WRITE_ADDRESS_CHECKING
-endif
-ifeq ($(HFRISCV_READ_ADDRESS_CHECKING), YES)
-	COMPLINE := $(COMPLINE) -DHFRISCV_READ_ADDRESS_CHECKING
-endif
-ifeq ($(HFRISCV_ENABLE_COUNTERS), YES)
-	COMPLINE := $(COMPLINE) -DHFRISCV_ENABLE_COUNTERS
-endif
-ifeq ($(HFRISCV_MODE), CYCLE)
-	COMPLINE := $(COMPLINE) -DHFRISCV_CYCLE_ACCURACY 
-endif
-
-#router parameters
-ifeq ($(ROUTER_ENABLE_COUNTERS), YES)
-	COMPLINE := $(COMPLINE) -DROUTER_ENABLE_COUNTERS
-endif
-ifeq ($(ROUTER_PORT_CONNECTED_CHECKING), YES)
+#router
+ifeq ($(ORCA_GROUNDED_PORTS_CHECKING), YES)
 	COMPLINE := $(COMPLINE) -DROUTER_PORT_CONNECTED_CHECKING
 endif
 
+#off-chip
+ifeq ($(ORCA_OFFCHIP_LOG_IN), YES)
+	COMPLINE := $(COMPLINE) -DNETBRIDGE_ENABLE_LOG_INPUT
+endif
+ifeq ($(ORCA_OFFCHIP_LOG_OUT) ), YES)
+	COMPLINE := $(COMPLINE) -DNETBRIDGE_ENABLE_LOG_OUTPUT
+endif
+
+COMPLINE := $(COMPLINE) \
+	-DNETSOCKET_CLIENT_ADDRESS=$(ORCA_OFFCHIP_CLIENT_IP) \
+	-DNETSOCKET_CLIENT_PORT=$(ORCA_OFFCHIP_CLIENT_PORT) \
+	-DNETSOCKET_SERVER_ADDRESS=$(ORCA_OFFCHIP_SERVER_IP) \
+	-DNETSOCKET_SERVER_PORT=$(ORCA_OFFCHIP_SERVER_PORT)
+
+#memory
+COMPLINE := $(COMPLINE) \
+	-DORCA_MEMORY_BASE=$(ORCA_MEMORY_BASE) \
+	-DORCA_MEMORY_SIZE=$(ORCA_MEMORY_SIZE)
+
+ifeq ($(ORCA_MEMORY_SPACE_CHECKING), YES)
+	COMPLINE := $(COMPLINE) \
+		-DMEMORY_WRITE_ADDRESS_CHECKING \
+		-DMEMORY_READ_ADDRESS_CHECKING \
+		-DMEMORY_WIPE_ADDRESS_CHECKING
+endif
+
+#riscv
+ifeq ($(ORCA_HFRISCV_MODE), CYCLE)
+	COMPLINE := $(COMPLINE) -DHFRISCV_CYCLE_ACCURACY 
+endif
+
+#counters
+ifeq ($(ORCA_HWCOUNTERS_MEMORY), YES)
+	COMPLINE := $(COMPLINE) -DMEMORY_ENABLE_COUNTERS
+endif
+
+ifeq ($(ORCA_HWCOUNTERS_HFRISCV), YES)
+	COMPLINE := $(COMPLINE) -DHFRISCV_ENABLE_COUNTERS
+endif
+#router parameters
+ifeq ($(ORCA_HWCOUNTERS_ROUTERS), YES)
+	COMPLINE := $(COMPLINE) -DROUTER_ENABLE_COUNTERS
+endif
+
 export COMPLINE
-export GLOBAL_SETTINGS
 export ORCA_NOC_HEIGHT
 export ORCA_NOC_WIDTH
-export ORCA_APPLICATIONS
-export ORCA_EXTENSIONS
-export KERNEL_LOG_LEVEL
--- Makefile
+++ Makefile
@@ -7,12 +7,12 @@ URSA_LIB      := libsim.a
 # Name of the library containing the hardware models. 
 MODELS_LIB    := libmod.a
 
-# name of the library containg the GDB RSP
+# name of the library containg the GDB RSP implementation.
 GDBRSP_LIB    := libgdbrsp.a
 
 # Additional parameters (do not modify them unless you know
 # what you are doing here).
-PLATFORM_BIN      := $(PLATFORM).exe
+PLATFORM_BIN  := $(ORCA_PLATFORM).exe
 
 # Be silent per default, but 'make V=1' will show all compiler calls.
 ifneq ($(V),1)
@@ -21,16 +21,11 @@ export Q := @
 MAKEFLAGS += --no-print-directory
 endif
 
-#include optmizations
-include ./Configuration.mk
-
 #directory configurations (paths)
 URSA_DIR      := $(CURDIR)/simulator
 BINARY_DIR    := $(CURDIR)/bin
 PLATFORMS_DIR := $(CURDIR)/platforms
 MODELS_DIR    := $(CURDIR)/models
-TOOLS_DIR     := $(CURDIR)/tools
-SOFTWARE_DIR  := $(CURDIR)/software
 GDBRSP_DIR    := $(CURDIR)/gdbrsp
 
 #phonies (see https://www.gnu.org/software/make/manual/html_node/Phony-Targets.html)
@@ -50,7 +45,7 @@ all: $(BINARY_DIR)/$(PLATFORM_BIN) vismtail
 	@echo "$'\e[7m====================================\e[0m"
 	@echo " => $(PLATFORM_BIN) deployed to /bin folder."
 
-#URSA's simulation library
+#Generate the simulation library (uses URSA distro)
 $(BINARY_DIR)/$(URSA_LIB): $(URSA_DIR)/src/*.cpp  $(URSA_DIR)/include/*.h 
 	@echo "$'\e[7m==================================\e[0m"
 	@echo "$'\e[7m     Building URSA's libsim       \e[0m"
@@ -58,7 +53,7 @@ $(BINARY_DIR)/$(URSA_LIB): $(URSA_DIR)/src/*.cpp  $(URSA_DIR)/include/*.h
 	$(Q)make -C $(URSA_DIR) -j 8
 	$(Q)cp $(URSA_DIR)/bin/$(URSA_LIB) $(BINARY_DIR)/$(URSA_LIB)
 
-#library containing hardware models
+#Generate the library containing the hardware models (except for top-level hardware)
 $(BINARY_DIR)/$(MODELS_LIB): $(BINARY_DIR)/$(URSA_LIB) $(MODELS_DIR)/src/*.cpp $(MODELS_DIR)/include/*.h
 	@echo "$'\e[7m==================================\e[0m"
 	@echo "$'\e[7m     Building hardware models     \e[0m"
@@ -66,7 +61,7 @@ $(BINARY_DIR)/$(MODELS_LIB): $(BINARY_DIR)/$(URSA_LIB) $(MODELS_DIR)/src/*.cpp $
 	$(Q)make -C $(MODELS_DIR) -j 8
 	$(Q)cp $(MODELS_DIR)/bin/$(MODELS_LIB) $(BINARY_DIR)/$(MODELS_LIB)
 
-#rsv module
+#Generate the GDB remote protocol library (used in processor core models). 
 $(BINARY_DIR)/$(GDBRSP_LIB): $(GDBRSP_DIR)/src/*.cpp $(GDBRSP_DIR)/include/*.h
 	@echo "$'\e[7m==================================\e[0m"
 	@echo "$'\e[7m   Building GDB RSV support lib.  \e[0m"
@@ -74,25 +69,26 @@ $(BINARY_DIR)/$(GDBRSP_LIB): $(GDBRSP_DIR)/src/*.cpp $(GDBRSP_DIR)/include/*.h
 	$(Q)make -C $(GDBRSP_DIR) -j 8
 	$(Q)cp $(GDBRSP_DIR)/bin/$(GDBRSP_LIB) $(BINARY_DIR)/$(GDBRSP_LIB)
 
-#platform executable
-$(BINARY_DIR)/$(PLATFORM_BIN): $(BINARY_DIR)/$(URSA_LIB) $(BINARY_DIR)/$(GDBRSP_LIB) $(BINARY_DIR)/$(MODELS_LIB) $(PLATFORMS_DIR)/$(PLATFORM)/src/*.cpp  $(PLATFORMS_DIR)/$(PLATFORM)/include/*.h
+#Generate simulator executable binary (includes top-level hardware models).
+#TODO: add  $(BINARY_DIR)/$(GDBRSP_LIB) to the dependency list
+$(BINARY_DIR)/$(PLATFORM_BIN): $(BINARY_DIR)/$(URSA_LIB) $(BINARY_DIR)/$(MODELS_LIB) $(PLATFORMS_DIR)/$(ORCA_PLATFORM)/src/*.cpp  $(PLATFORMS_DIR)/$(ORCA_PLATFORM)/include/*.h
 	@echo "$'\e[7m==================================\e[0m"
 	@echo "$'\e[7m     Building the platform        \e[0m"
 	@echo "$'\e[7m==================================\e[0m"
-	$(Q)make -C $(PLATFORMS_DIR)/$(PLATFORM) -j 8
-	$(Q)cp $(PLATFORMS_DIR)/$(PLATFORM)/bin/$(PLATFORM_BIN) $(BINARY_DIR)/$(PLATFORM_BIN)
+	$(Q)make -C $(PLATFORMS_DIR)/$(ORCA_PLATFORM) -j 8
+	$(Q)cp $(PLATFORMS_DIR)/$(ORCA_PLATFORM)/bin/$(PLATFORM_BIN) $(BINARY_DIR)/$(PLATFORM_BIN)
 
-#documentation
-#last line refers to a bug in tabu.sty. A replacement for
-#the file is provided within /tools folder and should be 
-#automatically applied while building
+#Make documentation by invoking doxygen using the provided doxyfile.
+#Generated documentation will be deployed to /docs folder.
 documentation:
 	@echo "$'\e[7m==================================\e[0m"
 	@echo "$'\e[7m    Building API Documentation    \e[0m"
 	@echo "$'\e[7m==================================\e[0m"
 	$(Q)doxygen
 
-#generete script for multitail (visualization, requires multitail)
+#Generate scripts for log visualization. For each processing core, a two log files are writen
+#to the /logs folder, one for UART output, and another one for Debugging output. Generated 
+#scripts will try to invoke multitail on these file, so make sure that multitail is installed.
 vismtail:
 	@echo "#!/bin/sh" > $(BINARY_DIR)/output-debug.sh
 	@echo "multitail ./logs/*debug.log -s $(ORCA_NOC_WIDTH)" \
@@ -107,8 +103,8 @@ clean:
 	@echo "$'\e[7m==================================\e[0m"
 	$(Q)make -C $(URSA_DIR) clean
 	$(Q)make -C $(MODELS_DIR) clean
-	$(Q)make -C $(RSVMOD_DIR) clean
-	$(Q)make -C $(PLATFORMS_DIR)/$(PLATFORM) clean
+	$(Q)make -C $(GDBRSP_DIR) clean
+	$(Q)make -C $(PLATFORMS_DIR)/$(ORCA_PLATFORM) clean
 	$(Q)rm -rf $(BINARY_DIR)/*.exe $(BINARY_DIR)/*.a $(BINARY_DIR)/*.o \
 		$(BINARY_DIR)/*~ $(BINARY_DIR)/*.elf $(BINARY_DIR)/*.bin \
 		$(BINARY_DIR)/*.cnt $(BINARY_DIR)/*.lst $(BINARY_DIR)/*.sec \
